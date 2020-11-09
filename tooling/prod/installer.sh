0580a6e6138b660d333fe65298f4a8b775a81487
mylesalamb <mylesalamb@gmail.com>
2020-11-09 00:12:12 +0000

    Finish deploy image and installer script

--- tooling/prod/installer.sh
+++ tooling/prod/installer.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-# Installer script for OCI images
+# Installer script for AMI images
 #       pull correct dependencies
 #       install, configure libraries
+#       The outputted applications is a ready to go version of the application
 
+NF_URL=https://www.netfilter.org/projects/libnetfilter_queue/files
+NF_TGT=libnetfilter_queue-1.0.5
+NF_EXT=.tar.bz2
 
+GREPO=https://github.com/mylesalamb/individualProject.git
+GREPO_PATH=individualProject/tooling/prod
 
-wget https://www.netfilter.org/projects/libnetfilter_queue/files/libnetfilter_queue-1.0.5.tar.bz2
-tar -xvf libnetfilter_queue-1.0.5.tar.bz2
+sudo apt-get update -y
+sudo apt-get install -y gcc make cmake wget libmnl-dev libnfnetlink-dev libpcap-dev 
 
-cd libnetfilter_queue-1.0.5
+wget "${NF_URL}/${NF_TGT}${NF_EXT}"
+tar -xvf "${NF_TGT}${NF_EXT}"
+
+cd $NF_TGT
 ./configure
 make
-make install
-echo "$PWD" >> /etc/ld.so.conf
+sudo make install
 cd ..
+
+git clone $GREPO
+cd $GREPO_PATH
+make
 
 
 make
