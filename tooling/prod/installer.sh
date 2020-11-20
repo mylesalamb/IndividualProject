9f654891f5d38a672fdba9155b9811695fabdc36
mylesalamb <mylesalamb@gmail.com>
2020-11-20 18:56:13 +0000

    Add quic to install.sh
    * added quic setup to installer to build lsquic as a static library

--- tooling/prod/installer.sh
+++ tooling/prod/installer.sh
@@ -13,10 +13,18 @@ NF_PATH=/usr/local/lib
 GREPO=https://github.com/mylesalamb/individualProject.git
 GREPO_PATH=individualProject/tooling/prod
 
+BSSLREPO=https://boringssl.googlesource.com/boringssl
+BSSL_PATH=boringssl
+
+LSREPO=https://github.com/litespeedtech/lsquic.git
+LSREPO_PATH=lsquic
+LSREPO_SHA=b117a3a0b7bd11fe6ebd503ec6b45d6b910b41a1
 # Update image to most recent version, and install
 # non crit dependencies
 sudo apt-get update -y
-sudo apt-get install -y gcc make cmake wget libmnl-dev libnfnetlink-dev libpcap-dev 
+sudo apt-get install -y gcc g++ zlib1g-dev golang make cmake wget libmnl-dev libnfnetlink-dev libpcap-dev libevent-dev
+
+cd ~
 
 # pull netfilter 1.0.5
 wget "${NF_URL}/${NF_TGT}${NF_EXT}"
@@ -37,11 +45,27 @@ else
 	. ~/.bashrc
 fi
 
-cd ..
+# setup boring ssl (lsquic dependency)
+cd ~
+git clone $BSSLREPO
+cd $BSSL_PATH
+git checkout $LSREPO_SHA
+cmake . && make
+BORINGSSL=$PWD
 
+
+# Setup lsquic library
+cd ~
+git clone $LSREPO
+cd $LSREPO_PATH
+git submodule init
+git submodule update 
+cmake -DBORINGSSL_DIR=$BORINGSSL -DBORINGSSL_INCLUDE=$BORINGSSL/include . && make
+# TODO:	add cronjjob to run the dataset once daily
+#		Save ip(6)tables rules so that we dont have to use root at runtime
+
+# Setup the ecn tool
+cd ~
 git clone $GREPO
 cd $GREPO_PATH
 make
-
-# TODO:	add cronjjob to run the dataset once daily
-#		Save ip(6)tables rules so that we dont have to use root at runtime
