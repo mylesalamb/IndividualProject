caf37b12d1a4a58029202116c5de01d6056d4510
mylesalamb <mylesalamb@gmail.com>
2020-11-29 23:50:23 +0000

    finish up with terrafrom config
    The actual terraform part of the project is more or less done
    we can scale up to a massive deployment with aws instances that are
    aware of where they are, all that is left to do is tweak packer/cron
    to schedule jobs when we actually need to go

--- tooling/terraform-shared-config/module.tf
+++ tooling/terraform-shared-config/module.tf
@@ -10,13 +10,26 @@ provider "aws" {
 resource "aws_instance" "ecnDetector" {
   provider                    = aws.singleregion
   ami                         = var.ami_image
-  # key_name                    = var.aws_key_name
-  instance_type               = "t2.nano"
+  key_name                    = var.aws_key_name
+  instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.default.id]
   subnet_id                   = aws_subnet.default.id
   associate_public_ip_address = true
   source_dest_check           = false
+  provisioner "remote-exec" {
+    inline = [
+      "echo \"AWS_REGION=${var.region}\" >> /home/ubuntu/.bashrc"
+    ]
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      host = self.public_ip
+      private_key = file("~/masterKey.pem")
+    }
+  }
 }
+
+
 
 output "instance_ip" {
   description = "Public ip addr"
--- tooling/terraform-shared-config/variables.tf
+++ tooling/terraform-shared-config/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {}
 variable "ami_image" {}
 
-# variable "aws_key_path" { default = "~./vmKey.pem" }
-# variable "aws_key_name" { default = "vmKey.pem" }
+variable "aws_key_name" { default = "masterKey" }
+# variable "aws_key_contents" {default = file("~/masterKey.pem")}
