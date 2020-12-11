7b851dbc505f139d06ac7b41f293e763f07cf715
mylesalamb <mylesalamb@gmail.com>
2020-12-11 14:31:40 +0000

    Begin to fix ci pipeline

--- tooling/datasets/formatWebDataset.sh
+++ tooling/datasets/formatWebDataset.sh
@@ -8,12 +8,8 @@ cat "web.raw.dataset" | while read line; do
 
 	elt=$(dig +short $line A | grep -v '\.$' | head -n 1)
 	if [  $? -eq 0  ] && [ ! -z "$elt" ]; then
-	
-		
   			echo "$elt WEB $line"
-		
 	fi
-
 	elt=$(dig +short $line AAAA | grep -v '\.$' | head -n 1)
 	if [  $? -eq 0  ] && [ ! -z "$elt" ]; then
   			echo "$elt WEB $line"
