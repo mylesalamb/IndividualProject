d312d8d77a91623cf3e3ac02aeed9827e712c80c
mylesalamb <mylesalamb@gmail.com>
2021-01-03 22:54:02 +0000

    add some experiment runners

---
+++ tooling/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# init script as a start time for the experiment
+# should be set to run at midnight via a cron job, will dynamically remove the previous job
+# and add in the proper schedule
+
+cd "$(dirname "$0")"
+
+# remove init job to start at midnight (ie. this job)
+crontab -u pi -l | grep -v "init.sh" | crontab -u pi -
+#add job to run on n hour schedule
+(crontab -u pi -l; echo "0 */36 * * * $(pwd)/prod/run_experiment.sh")
+
+./run_experiment.sh
