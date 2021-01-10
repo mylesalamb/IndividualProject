9c3ac47161481ac0e163e39fa895f888d90c7e18
mylesalamb <mylesalamb@gmail.com>
2021-01-10 15:04:36 +0000

    stuff stuff

--- tooling/init.sh
+++ tooling/init.sh
@@ -6,8 +6,8 @@
 cd "$(dirname "$0")"
 
 # remove init job to start at midnight (ie. this job)
-crontab -u pi -l | grep -v "init.sh" | crontab -u pi -
+crontab -l | grep -v "init.sh" | crontab -
 #add job to run on n hour schedule
-(crontab -u pi -l; echo "0 0 */2 * * $(pwd)/prod/run_experiment.sh") | crontab -u pi -
+(crontab -l; echo "0 0 */2 * * $(pwd)/prod/run_experiment.sh") | crontab -
 
 ./prod/run_experiment.sh
