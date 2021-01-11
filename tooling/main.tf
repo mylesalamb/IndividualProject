f5e28f249446248615d72d1cd62971f008b69ecf
mylesalamb <mylesalamb@gmail.com>
2021-01-11 11:28:37 +0000

    Add terraform high level config for deployment

--- tooling/main.tf
+++ tooling/main.tf
@@ -3,19 +3,19 @@ terraform {
 }
 
 # Populate all of these from packerwrapper.py -> *.tfvars.json
-# variable us-east-1_ami      {}
+variable us-east-1_ami      {}
 # variable us-east-2_ami      {}
-# variable us-west-1_ami      {}
+variable us-west-1_ami      {}
 # variable us-west-2_ami      {}
 
 # variable ap-east-1_ami      {}
-# variable ap-northeast-1_ami {}
+variable ap-northeast-1_ami {}
 # variable ap-northeast-2_ami {}
 # variable ap-southeast-1_ami {}
 # variable ap-southeast-2_ami {}
 # variable ap-south-1_ami     {}
 
-variable eu-west-1_ami      {}
+# variable eu-west-1_ami      {}
 variable eu-west-2_ami      {}
 # variable eu-west-3_ami      {}
 # variable eu-central-1_ami   {}
@@ -23,16 +23,16 @@ variable eu-west-2_ami      {}
 # variable eu-north-1_ami     {}
 
 # variable ca-central-1_ami   {}
-# variable me-south-1_ami     {}
-# variable sa-east-1_ami      {}
+variable me-south-1_ami     {}
+variable sa-east-1_ami      {}
 
 # US
 
-# module "us-west-1" {
-#   source = "./terraform-shared-config"
-#   region = "us-west-1"
-#   ami_image = var.us-west-1_ami
-# }
+module "us-west-1" {
+  source = "./terraform-shared-config"
+  region = "us-west-1"
+  ami_image = var.us-west-1_ami
+}
 
 # module "us-west-2" {
 #   source = "./terraform-shared-config"
@@ -40,11 +40,11 @@ variable eu-west-2_ami      {}
 #   ami_image = var.us-west-2_ami
 # }
 
-# module "us-east-1" {
-#   source = "./terraform-shared-config"
-#   region = "us-east-1"
-#   ami_image = var.us-east-1_ami
-# }
+module "us-east-1" {
+  source = "./terraform-shared-config"
+  region = "us-east-1"
+  ami_image = var.us-east-1_ami
+}
 
 # module "us-east-2" {
 #   source = "./terraform-shared-config"
@@ -54,11 +54,11 @@ variable eu-west-2_ami      {}
 
 # # Asia/pacific
 
-# module "ap-northeast-1" {
-#   source = "./terraform-shared-config"
-#   region = "ap-northeast-1"
-#   ami_image = var.ap-northeast-1_ami
-# }
+module "ap-northeast-1" {
+  source = "./terraform-shared-config"
+  region = "ap-northeast-1"
+  ami_image = var.ap-northeast-1_ami
+}
 
 # module "ap-northeast-2" {
 #   source = "./terraform-shared-config"
@@ -93,11 +93,11 @@ variable eu-west-2_ami      {}
 
 # EU
 
-module "eu-west-1" {
-  source = "./terraform-shared-config"
-  region = "eu-west-1"
-  ami_image = var.eu-west-1_ami
-}
+# module "eu-west-1" {
+#   source = "./terraform-shared-config"
+#   region = "eu-west-1"
+#   ami_image = var.eu-west-1_ami
+# }
 
 module "eu-west-2" {
   source = "./terraform-shared-config"
@@ -138,14 +138,14 @@ module "eu-west-2" {
 #   ami_image = var.ca-central-1_ami
 # }
 
-# module "me-south-1" {
-#   source = "./terraform-shared-config"
-#   region = "me-south-1"
-#   ami_image = var.me-south-1_ami
-# }
+ module "me-south-1" {
+   source = "./terraform-shared-config"
+   region = "me-south-1"
+   ami_image = var.me-south-1_ami
+ }
 
-# module "sa-east-1" {
-#   source = "./terraform-shared-config"
-#   region = "sa-east-1"
-#   ami_image = var.sa-east-1_ami
-# }
+module "sa-east-1" {
+  source = "./terraform-shared-config"
+  region = "sa-east-1"
+  ami_image = var.sa-east-1_ami
+}
